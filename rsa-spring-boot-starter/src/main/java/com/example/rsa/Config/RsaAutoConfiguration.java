f9171df537949b97ef44478268f627f738894422
范志超 <fanzhichao@MacBook-Pro.local>
2021-06-01 15:01:01 +0800

    update

---
+++ rsa-spring-boot-starter/src/main/java/com/example/rsa/Config/RsaAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.rsa.Config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("com.example.rsa")
+public class RsaAutoConfiguration {
+}
